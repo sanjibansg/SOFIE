a9c330c8293d328714c578974adfa8be303ffb3e
Harsh Chauhan <harsh2005.hc@gmail.com>
2026-06-16 03:03:11 +0530

    add gtest for selu gpu support

--- test/alpaka/TestAlpakaElementwiseUnary.cxx
+++ test/alpaka/TestAlpakaElementwiseUnary.cxx
@@ -16,6 +16,8 @@
 #include "Softplus_FromONNX_GPU_ALPAKA.hxx"
 #include "Elu_FromONNX_GPU_ALPAKA.hxx"
 #include "input_models/references/Elu.ref.hxx"
+#include "Selu_FromONNX_GPU_ALPAKA.hxx"
+#include "input_models/references/Selu.ref.hxx"
 
 TEST_F(SofieAlpakaTest, Sin)
 {
@@ -352,6 +354,39 @@ TEST_F(SofieAlpakaTest, Elu)
 
     float* res_ptr = reinterpret_cast<float*>(alpaka::getPtrNative(result_h));
     float* correct = Elu_ExpectedOutput::outputs;
+    for (size_t i = 0; i < nOut; ++i) {
+        EXPECT_LE(std::abs(res_ptr[i] - correct[i]), TOLERANCE) << "i=" << i;
+    }
+}
+
+TEST_F(SofieAlpakaTest, Selu)
+{
+    constexpr float TOLERANCE = DEFAULT_TOLERANCE;
+
+    std::vector<float> input({1.0f, -2.0f, 3.0f, 0.5f, -1.0f, 2.0f});
+
+    auto input_h = alpaka::allocBuf<float, Idx>(host, Ext1D::all(Idx{input.size()}));
+    float* input_ptr = reinterpret_cast<float*>(alpaka::getPtrNative(input_h));
+    for (Idx i = 0; i < input.size(); ++i) input_ptr[i] = input[i];
+
+    auto input_d = alpaka::allocBuf<float, Idx>(device, Ext1D::all(Idx{input.size()}));
+    alpaka::memcpy(queue, input_d, input_h);
+    alpaka::wait(queue);
+
+    constexpr size_t nOut = sizeof(Selu_ExpectedOutput::outputs) / sizeof(float);
+    auto result_h = alpaka::allocBuf<float, Idx>(host, Ext1D::all(Idx{nOut}));
+
+    {
+        SOFIE_Selu::Session<alpaka::TagGpuCudaRt> session;
+        auto result = session.infer(input_d);
+        alpaka::wait(queue);
+        cudaDeviceSynchronize();
+        alpaka::memcpy(queue, result_h, result);
+        alpaka::wait(queue);
+    }
+
+    float* res_ptr = reinterpret_cast<float*>(alpaka::getPtrNative(result_h));
+    float* correct = Selu_ExpectedOutput::outputs;
     for (size_t i = 0; i < nOut; ++i) {
         EXPECT_LE(std::abs(res_ptr[i] - correct[i]), TOLERANCE) << "i=" << i;
     }
---
+++ test/input_models/references/Selu.ref.hxx
@@ -0,0 +1,5 @@
+// Auto-generated SELU reference - DO NOT EDIT
+#pragma once
+namespace Selu_ExpectedOutput {
+   float outputs[] = {1.05070102f, -1.52016652f, 3.15210295f, 0.52535051f, -1.11133075f, 2.10140204f};
+} // namespace Selu_ExpectedOutput
