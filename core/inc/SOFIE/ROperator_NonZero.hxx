503fcca8c9ed8db4bf107b9ae5371422dc957548
Antreas Ioannou <ioannouandreas2003@gmail.com>
2026-08-28 17:28:34 +0200

    fix: address Alpaka operator review comments

--- core/inc/SOFIE/ROperator_NonZero.hxx
+++ core/inc/SOFIE/ROperator_NonZero.hxx
@@ -84,10 +84,8 @@ public:
       op += SP + "struct NonZeroKernel_" + fNY + " {\n";
       op += SP + SP + "template<typename TAcc, typename T>\n";
       op += SP + SP + "ALPAKA_FN_ACC void operator()(TAcc const& acc, T const* input, int64_t* output, std::size_t* count) const {\n";
-      op += SP + SP + SP + "auto const i = alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0];\n";
-      op += SP + SP + SP + "if (i != 0u) return;\n";
       op += SP + SP + SP + "std::size_t n = 0;\n";
-      op += SP + SP + SP + "for (std::size_t j = 0; j < " + std::to_string(fInputLength) + "u; ++j) if (input[j] != static_cast<T>(0)) ++n;\n";
+      op += SP + SP + SP + "for (std::size_t j = 0; j < " + std::to_string(fInputLength) + "u; ++j) n += input[j] != static_cast<T>(0);\n";
       op += SP + SP + SP + "*count = n;\n";
       op += SP + SP + SP + "std::size_t nz = 0;\n";
       op += SP + SP + SP + "for (std::size_t j = 0; j < " + std::to_string(fInputLength) + "u; ++j) {\n";
@@ -118,7 +116,8 @@ public:
       out << SP << "auto nonZeroCountDev_" << fNY << " = alpaka::allocBuf<std::size_t, Idx>(devAcc, Ext1D::all(Idx{1}));\n";
       out << SP << "auto nonZeroCountHost_" << fNY << " = alpaka::allocBuf<std::size_t, Idx>(hostAcc, Ext1D::all(Idx{1}));\n";
       out << SP << "auto const workDivNonZero_" << fNY << " = sofie_workdiv(Vec::all(Idx{1}));\n";
-      out << SP << "alpaka::exec<Acc>(queue, workDivNonZero_" << fNY << ", nonZeroKernel_" << fNY << ", alpaka::getPtrNative(deviceBuf_" << fNX << "), alpaka::getPtrNative(bufDev_" << fNY << "), alpaka::getPtrNative(nonZeroCountDev_" << fNY << "));\n";
+      out << SP << "auto taskNonZero_" << fNY << " = alpaka::createTaskKernel<Acc>(workDivNonZero_" << fNY << ", nonZeroKernel_" << fNY << ", alpaka::getPtrNative(deviceBuf_" << fNX << "), alpaka::getPtrNative(bufDev_" << fNY << "), alpaka::getPtrNative(nonZeroCountDev_" << fNY << "));\n";
+      out << SP << "alpaka::enqueue(queue, taskNonZero_" << fNY << ");\n";
       out << SP << "alpaka::memcpy(queue, nonZeroCountHost_" << fNY << ", nonZeroCountDev_" << fNY << ");\n";
       out << SP << "alpaka::wait(queue);\n";
       out << SP << "size_t " << fCountName << " = *alpaka::getPtrNative(nonZeroCountHost_" << fNY << ");\n";
